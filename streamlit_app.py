a003b4ee95bc16fc8e227a2f09a473c454274523
金井伸也 <kanaishinya@skanaiMBA.local>
2022-12-11 16:08:01 +0900

    check packages fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import pkg_resources
+packages = []
 for _lib in pkg_resources.working_set:
-    print(_lib.project_name, _lib.version)
+    # print(_lib.project_name, _lib.version)
+    packages.append((_lib.project_name,_lib.version))
+
+packages
