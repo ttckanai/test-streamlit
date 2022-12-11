b9c198a6cf084bf95a7efe4ac289c6162740e985
金井伸也 <kanaishinya@skanaiMBA.local>
2022-12-11 16:54:08 +0900

    display python version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import pkg_resources
+import sys
+
 packages = []
 for _lib in pkg_resources.working_set:
     # print(_lib.project_name, _lib.version)
     packages.append((_lib.project_name,_lib.version))
 
 packages
+sys.version
